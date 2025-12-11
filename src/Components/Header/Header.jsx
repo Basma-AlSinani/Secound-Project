1465850bd94dc98eaf16cfb25e9f875000191600
Basma Suliman <basmaalsinani97@gmail.com>
2025-12-11 10:22:32 +0400

    add image

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const Header = (props) => {
   return (
-    <div><h2>“If anyone bullies {props.title}, they must place this 50-baisa Omani metal coin in the cup on her desk.”</h2></div>
+    <div><h2>“If anyone bullies {props.title}, they must place this 50-baisa Omani metal coin in the cup on her desk.”</h2>
+
+      <img src="/IMG_8487.jpg" alt="my photo" width="200" />
+    </div>
   )
-};export default Header;
+}; export default Header;
